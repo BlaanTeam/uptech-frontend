f65287309ff589bd04dbec9591d988b9d7fa4380
Abdelhadi Sabani <abdo.sabani30@gmail.com>
2020-10-29 03:29:17 +0100

    Install: Cors

--- back-end/routes/index.js
+++ back-end/routes/index.js
@@ -6,6 +6,17 @@ var router = express.Router();
 
 router.get('/', function(req, res, next) {
   res.json({"msg":"Home Page"})
+
 });
+
+router.post('/getUsernameInfo',(req,res)=>{
+  res.status(404)
+  res.json(
+    {
+      username:req.body.username,
+      status:"Not Found"
+    }
+  )
+})
 
 module.exports = router;
