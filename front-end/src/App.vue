3637793286681e11becc8edfec19f0aebbe2d5de
abdelouahed <doonabdo98@gmail.com>
2020-10-29 16:30:11 +0000

    Refactor: remove all of the boilerplate code

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,87 +1,12 @@
 <template>
   <v-app>
-    <v-app-bar class="test" app color="primary" dark>
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer ></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-main>
-      <HelloWorld />
-    </v-main>
+    <v-main> </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld";
-
 export default {
-  name: "App",
-
-  components: {
-    HelloWorld
-  },
-
-  created() {
-    this.$http
-      .get("/")
-      .then(resp => {
-        console.log(resp.data);
-      })
-      .catch(err => {
-        console.log(err.response);
-      });
-  },
-
-  data: () => ({
-    //
-  }),
-  module: {
-    rules: [
-      {
-        test: /\.scss$/,
-        use: [
-          'vue-style-loader',
-          'css-loader',
-          'sass-loader'
-        ]
-      }
-    ]
-  },
+  name: "App"
 };
-
-
-
 </script>
-<style lang="scss">
-
-
-</style>
-
+<style lang="scss"></style>
