57c01ae815dbc4c02fb9f311c8a598ccc3dc5b82
Abdelhadi Sabani <abdo.sabani30@gmail.com>
2020-10-30 18:05:50 +0100

    Update: Enhace Code

--- front-end/src/layouts/Default.vue
+++ front-end/src/layouts/Default.vue
@@ -1,17 +1,18 @@
 <template>
   <v-app dark>
     <Nav />
+    <AppBar />
     <slot></slot>
   </v-app>
 </template>
 
 <script>
 import Nav from "@/layouts/partials/Nav";
+import AppBar from "../components/Navigation/AppBar";
 export default {
   components: {
-    Nav
+    Nav,
+    AppBar
   }
 };
 </script>
-
-<style></style>
