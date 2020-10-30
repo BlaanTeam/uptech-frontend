479696e853732963e9a189a184944ec88cf42fdd
Abdelhadi Sabani <abdo.sabani30@gmail.com>
2020-10-30 18:41:07 +0100

    Add: Carousel

--- front-end/src/views/Home/Home.vue
+++ front-end/src/views/Home/Home.vue
@@ -1,11 +1,25 @@
 <template>
   <div class="home">
-    Hello From Home Page
+    <v-carousel v-model="model" continuous>
+      <v-carousel-item v-for="(color, i) in colors" :key="color">
+        <v-sheet :color="color" height="100%" tile>
+          <v-row class="fill-height" align="center" justify="center">
+            <div class="display-3">Slide {{ i + 1 }}</div>
+          </v-row>
+        </v-sheet>
+      </v-carousel-item>
+    </v-carousel>
   </div>
 </template>
 
 <script>
 export default {
-  name: "Home"
+  name: "Home",
+  data() {
+    return {
+      model: 0,
+      colors: ["primary", "secondary", "yellow darken-2", "red", "orange"]
+    };
+  }
 };
 </script>
