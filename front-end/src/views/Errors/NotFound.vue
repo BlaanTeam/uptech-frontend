a10e6e32e2713ff21c6129cfeed6bf9479bd701b
Abdelhadi Sabani <abdo.sabani30@gmail.com>
2020-10-30 14:59:23 +0100

    Add: Not Found Page

---
+++ front-end/src/views/Errors/NotFound.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="not-found">
+    <v-container fluid>
+      <v-layout class="d-flex flex-column ">
+        <p class="display-3 text-center">WHOOPS !</p>
+        <p class="display-5 text-center">
+          The page you are trying to get to never existed in this reality, or
+          has migrated to a parallel universe.
+        </p>
+        <v-img src="@/assets/images/not_found.svg" class="align-self-center">
+        </v-img>
+      </v-layout>
+    </v-container>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
