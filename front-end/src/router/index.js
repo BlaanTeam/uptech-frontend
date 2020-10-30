1a323f3a594e1a510115aba7df9ecb1255b59f5d
Abdelhadi Sabani <abdo.sabani30@gmail.com>
2020-10-30 15:00:01 +0100

    Add: Titles To Routes

--- front-end/src/router/index.js
+++ front-end/src/router/index.js
@@ -2,25 +2,62 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home/Home.vue";
 import About from "../views/Home/About.vue";
+import NotFound from "../views/Errors/NotFound.vue";
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: "/",
     name: "Home",
-    component: Home
+    component: Home,
+    meta: {
+      title: "Home"
+    }
   },
   {
     path: "/about",
     name: "About",
-    component: About
+    component: About,
+    meta: {
+      title: "About Us"
+    }
+  },
+  {
+    path: "/sign_in",
+    name: "SignIn",
+    // component:,
+    meta: {
+      title: "Sign In"
+    }
+  },
+  {
+    path: "/sign_up",
+    name: "SignUp",
+    // component:,
+    meta: {
+      title: "Sing Up"
+    }
+  },
+  {
+    path: "*",
+    name: "NotFound",
+    component: NotFound,
+    meta: {
+      title: "Not Found"
+    }
   }
 ];
 
 const router = new VueRouter({
-  mode: "history",
+  mode: false,
   base: process.env.BASE_URL,
   routes
+});
+
+router.afterEach((to, from) => {
+  Vue.nextTick(() => {
+    document.title = "UpTech | " + (to.meta.title || "Welcome");
+  });
 });
 
 export default router;
