456c6c40db676434fa4d6f039fc46782dee827b0
Abdelhadi Sabani <abdo.sabani30@gmail.com>
2020-10-30 18:29:25 +0100

    Update: Add Snackbar

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -3,6 +3,15 @@
     <v-main>
       <v-container fluid>
         <router-view></router-view>
+        <v-snackbar v-model="snackbar">
+          {{ text }}
+
+          <template v-slot:action="{ attrs }">
+            <v-btn color="pink" text v-bind="attrs" @click="snackbar = false">
+              Close
+            </v-btn>
+          </template>
+        </v-snackbar>
       </v-container>
     </v-main>
   </Layout>
@@ -10,7 +19,16 @@
 
 <script>
 export default {
-  name: "App"
+  name: "App",
+  created() {
+    this.snackbar = true;
+  },
+  data() {
+    return {
+      snackbar: false,
+      text: "Welcome To UpTech"
+    };
+  }
 };
 </script>
 <style lang="scss"></style>
