83883a541c2483f20b0ba6ead3f2249ab5c95989
Abdelhadi Sabani <abdo.sabani30@gmail.com>
2020-10-31 20:30:32 +0100

    Update : Clean App

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,34 +1,14 @@
 <template>
   <Layout>
     <v-main>
-      <v-container fluid>
-        <router-view></router-view>
-        <v-snackbar v-model="snackbar">
-          {{ text }}
-
-          <template v-slot:action="{ attrs }">
-            <v-btn color="pink" text v-bind="attrs" @click="snackbar = false">
-              Close
-            </v-btn>
-          </template>
-        </v-snackbar>
-      </v-container>
+      <router-view></router-view>
     </v-main>
   </Layout>
 </template>
 
 <script>
 export default {
-  name: "App",
-  created() {
-    this.snackbar = true;
-  },
-  data() {
-    return {
-      snackbar: false,
-      text: "Welcome To UpTech"
-    };
-  }
+  name: "App"
 };
 </script>
 <style lang="scss"></style>
