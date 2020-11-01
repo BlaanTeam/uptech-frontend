5998c5a99c11bdacae805194d77f986c4225ed2a
Abdelhadi Sabani <abdo.sabani30@gmail.com>
2020-11-02 00:03:51 +0100

    Add: Home Page

--- front-end/src/views/Home/Home.vue
+++ front-end/src/views/Home/Home.vue
@@ -1,5 +1,20 @@
 <template>
-  <v-container fluid class="home"> </v-container>
+  <v-container fluid class="home">
+    <v-layout class="d-fex flex-column">
+      <h1 class="align-self-center">
+        Welcome to our webiste feel free to join us
+      </h1>
+      <div class="text-center">
+        <v-btn class="ma-2" color="success">
+          Sign In
+        </v-btn>
+
+        <v-btn class="ma-2" color="info">
+          Sign Up
+        </v-btn>
+      </div>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
@@ -10,3 +25,9 @@ export default {
   }
 };
 </script>
+
+<style lang="scss" scoped>
+.home {
+  padding: unset;
+}
+</style>
