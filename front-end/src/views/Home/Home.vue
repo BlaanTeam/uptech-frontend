4e494b2c5a21ed97b3c1847737b6fd7c52b23139
Abdelhadi Sabani <abdo.sabani30@gmail.com>
2020-11-01 23:17:44 +0100

    Update: Clean Code

--- front-end/src/views/Home/Home.vue
+++ front-end/src/views/Home/Home.vue
@@ -1,25 +1,12 @@
 <template>
-  <div class="home">
-    <v-carousel v-model="model" style="height:100%">
-      <v-carousel-item v-for="(color, i) in colors" :key="color">
-        <v-sheet :color="color" height="100%" tile>
-          <v-row class="fill-height" align="center" justify="center">
-            <div class="display-3">Slide {{ i + 1 }}</div>
-          </v-row>
-        </v-sheet>
-      </v-carousel-item>
-    </v-carousel>
-  </div>
+  <v-container fluid class="home"> </v-container>
 </template>
 
 <script>
 export default {
   name: "Home",
   data() {
-    return {
-      model: 2,
-      colors: ["primary", "secondary", "yellow darken-2", "red", "orange"]
-    };
+    return {};
   }
 };
 </script>
