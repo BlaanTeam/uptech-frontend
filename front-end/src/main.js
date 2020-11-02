4ab7449bf193e11429163c405129635c329386e9
abdelouahed <doonabdo98@gmail.com>
2020-11-02 14:16:05 +0000

    Add: assets/css

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -4,7 +4,7 @@ import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
 import "./plugins/axios";
-import "@/assets/style/index.scss";
+import "@/assets/css/index.scss";
 
 Vue.config.productionTip = false;
 
