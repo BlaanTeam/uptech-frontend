b5d46778cdd94626b3cbe91d8e55e7f766d4d927
abdelouahed <doonabdo98@gmail.com>
2020-11-04 14:33:45 +0000

    update: home page

--- front-end/src/views/Home/Home.vue
+++ front-end/src/views/Home/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-container fluid class="home pa-0">
+  <v-container fluid class="home pa-0 white--text">
     <section class="first-section d-flex align-center pa-4">
       <v-layout class="first-section__background"></v-layout>
       <v-layout class="first-section__header flex-column align-center mt-10">
@@ -15,31 +15,16 @@
             rounded
             dark
             class="ma-4 mr-6 py-6 px-12"
-            color="amber darken-3"
+            color="#e08c04"
           >
-            Sign In
-          </v-btn>
-
-          <v-btn
-            elevation="0"
-            rounded
-            dark
-            class="ma-4 py-6 px-12"
-            color="blue lighten-1"
-          >
-            Sign Up
+            Create an account
           </v-btn>
         </v-layout>
         <div class="display-5 ma-4 mt-15 ">
           I'm curious, just want to take a look
         </div>
         <v-layout>
-          <v-btn
-            elevation="1"
-            rounded
-            class="py-5 px-10"
-            color="amber lighten-2"
-          >
+          <v-btn elevation="1" rounded class="py-5 px-10" color="#1f8a7d" dark>
             Login as guest
           </v-btn>
         </v-layout>
@@ -68,8 +53,8 @@ export default {
 
 <style lang="scss" scoped>
 .first-section {
-  background: #f3cd7c;
-  min-height: 90vh;
+  background: #1a796e;
+  min-height: 91vh;
   &__title {
     z-index: 1;
     // color: white;
@@ -81,7 +66,8 @@ export default {
     top: 0;
     left: 0;
     clip-path: polygon(100% 0, 100% 13%, 0 50%, 0 0);
-    background: #d1ecf3;
+    background: #1e3a46;
+    // background: linear-gradient(0deg, rgb(16, 21, 24) 0%, rgb(47, 52, 58) 100%);
   }
 }
 </style>
