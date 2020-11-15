e6d25e4e8c39b50f1574bc96d80796f14c514618
Abdelouahed Oumoussa <abdelwahd.oumouss@gmail.com>
2020-11-15 01:26:19 +0100

    Install: vue notifications plugin

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -1,12 +1,16 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import patterns from "./store/patterns";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
 import "./plugins/axios";
 import i18n from "./i18n";
+import Notifications from "vue-notification";
 
+Vue.use(Notifications);
 Vue.prototype.$theme = vuetify.framework.theme;
+
 Vue.config.productionTip = false;
 
 new Vue({
