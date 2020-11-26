69adcd2b5175e15a76068bac6ee2c7b805e179ea
Abdelouahed Oumoussa <abdelwahd.oumouss@gmail.com>
2020-11-26 18:29:04 +0100

    Update: NotFound.vue component

--- front-end/src/views/Errors/NotFound.vue
+++ front-end/src/views/Errors/NotFound.vue
@@ -1,10 +1,9 @@
 <template>
   <v-container fluid class="not-found pa-0 fill-height">
     <div class="d-flex flex-column mx-auto">
-      <p class="display-3 text-center">WHOOPS !</p>
+      <p class="display-3 text-center">{{ $t("notFound.h1") }}</p>
       <p class="display-5 text-center">
-        The page you are trying to get to never existed in this reality, or has
-        migrated to a parallel universe.
+        {{ $t("notFound.h2") }}
       </p>
       <v-spacer></v-spacer>
       <NotFoudSvg width="400" />
