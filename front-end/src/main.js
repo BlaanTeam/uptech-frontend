2967f327d77981a04401b2e9c7d6f6bd18bdc2a5
Abdelouahed Oumoussa <abdelwahd.oumouss@gmail.com>
2020-11-26 18:20:40 +0100

    Update: move i18n to pulgins directoy

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -5,7 +5,7 @@ import patterns from "./store/patterns";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
 import "./plugins/axios";
-import i18n from "./i18n";
+import i18n from "./plugins/i18n";
 import Notifications from "vue-notification";
 import Loading from "vue-loading-overlay";
 import "vue-loading-overlay/dist/vue-loading.css";
