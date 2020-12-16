883e6da76f6bce5cc0b998792ce7ae8ae81297ea
Abdelouahed Oumoussa <abdelwahd.oumouss@gmail.com>
2020-12-16 14:19:13 +0100

    Update: footer make it responsive

--- front-end/src/layouts/partials/Footer.vue
+++ front-end/src/layouts/partials/Footer.vue
@@ -1,9 +1,9 @@
 <template>
-  <v-footer color="bg lighten-1">
+  <v-footer color="bg footer lighten-1">
     <v-row>
-      <v-col cols="4" class="mx-10">
+      <v-col class="mx-10 socials">
         <h2 class="ml-4 primary--text">Get connected on</h2>
-        <div class="ml-6">
+        <div class="ml-6 buttons">
           <v-btn
             v-for="(social, i) in socials"
             :key="social + i"
@@ -14,9 +14,8 @@
           </v-btn>
         </div>
       </v-col>
-      <v-col class="mx-4">
-        <h2 class="ml-4 primary--text">Other links</h2>
-
+      <v-col>
+        <h2 class="ml-1 primary--text">Other links</h2>
         <v-list-item
           v-for="(link, i) in links"
           :key="link + i"
@@ -28,7 +27,7 @@
           </v-list-item-content>
         </v-list-item>
       </v-col>
-      <v-col class="mx-4">
+      <v-col>
         <h2 class="ml-4 primary--text">Support</h2>
         <router-link to="#" v-for="(support, i) in supports" :key="support + i">
           <v-list-item dense class="ma-0">
@@ -51,3 +50,19 @@ export default {
   })
 };
 </script>
+<style lang="scss">
+.footer {
+  z-index: 2;
+}
+// @media screen and (max-width: 700px) {
+//   .footer {
+//     .socials {
+//       margin: 0;
+//       padding: 0;
+//       display: block;
+//       background: red;
+//       width: 100%;
+//     }
+//   }
+// }
+</style>
