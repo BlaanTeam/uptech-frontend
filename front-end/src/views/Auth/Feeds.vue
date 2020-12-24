839e13a21984751604e6ff210a500b446b344172
Abdelouahed Oumoussa <abdelwahd.oumouss@gmail.com>
2020-12-24 23:51:36 +0100

    Add: Feeds View

---
+++ front-end/src/views/Auth/Feeds.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="feeds">
+    <CreatePost />
+    <Posts />
+  </div>
+</template>
+
+<script>
+import CreatePost from "@/components/AuthComponents/CreatePost";
+import Posts from "@/components/AuthComponents/Posts";
+
+export default {
+  components: { CreatePost, Posts }
+};
+</script>
+
+<style lang="scss"></style>
