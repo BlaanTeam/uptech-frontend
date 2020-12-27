f6d6763bf1f9e7ab6e0d72fa55fb5fe401cb4497
Abdelouahed Oumoussa <abdelwahd.oumouss@gmail.com>
2020-12-27 02:17:08 +0100

    Update: Feeds

--- front-end/src/views/Auth/Feeds.vue
+++ front-end/src/views/Auth/Feeds.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="feeds">
     <CreatePost />
-    <Posts />
+    <Posts :posts="posts" />
   </div>
 </template>
 
@@ -10,8 +10,29 @@ import CreatePost from "@/components/AuthComponents/CreatePost";
 import Posts from "@/components/AuthComponents/Posts";
 
 export default {
-  components: { CreatePost, Posts }
+  components: { CreatePost, Posts },
+  data: () => ({}),
+  methods: {},
+  computed: {
+    posts() {
+      return this.$store.getters.getPosts;
+    }
+  },
+  async created() {
+    try {
+      await this.$store.dispatch("getFeedPosts");
+    } catch (err) {
+      console.log(err);
+    }
+  }
 };
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+.feeds {
+  max-width: 700px;
+  height: 92vh;
+  padding: 16px 10px 40px 10px;
+  overflow-y: auto;
+}
+</style>
