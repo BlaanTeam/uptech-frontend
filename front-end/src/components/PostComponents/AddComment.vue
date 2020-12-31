bdbffed77299aa6d62b8bb0817f56f5269768f78
Abdelouahed Oumoussa <abdelwahd.oumouss@gmail.com>
2020-12-31 18:07:58 +0100

    Update: AddComment.vue -make the logic inside the component instead of using store

--- front-end/src/components/PostComponents/AddComment.vue
+++ front-end/src/components/PostComponents/AddComment.vue
@@ -1,23 +1,56 @@
 <template>
   <v-expand-transition>
-    <v-row v-show="display" class="me-2 ms-12 ma-0 pa-0">
+    <v-row
+      v-show="display"
+      :class="' me-2 ms-12 ma-0 pa-0 add-comment' + post._id"
+    >
       <v-col cols="1" class=" pa-0 ma-0">
         <v-avatar width="20" color="green" class="mt-3">
           <span class="white--text headline">me</span>
         </v-avatar>
       </v-col>
       <v-col cols="8" class="pa-0 ma-0 ms-2">
-        <div>
-          <v-textarea
-            autofocus
-            placeholder="What you do think"
-            label=""
-            rows="1"
-            row-height="10"
-            v-model="comment"
-            auto-grow
-          ></v-textarea>
-        </div>
+        <v-row no-gutters>
+          <v-col>
+            <v-textarea
+              ref="addCommentTextArea"
+              autofocus
+              placeholder="What you do think"
+              label=""
+              rows="1"
+              row-height="10"
+              v-model="comment"
+              auto-grow
+            ></v-textarea>
+          </v-col>
+          <v-col cols="1" class="align-self-center">
+            <v-menu
+              :close-on-content-click="false"
+              offset-y
+              left
+              :attach="'.add-comment' + post._id"
+              transition="slide-y-transition"
+            >
+              <template v-slot:activator="{ on, attrs }">
+                <v-btn
+                  v-bind="attrs"
+                  v-on="on"
+                  icon
+                  class="mx-1 float-right"
+                  color="white"
+                >
+                  😀
+                </v-btn>
+              </template>
+
+              <VEmojiPicker
+                :dark="$vuetify.theme.isDark"
+                @select="selectEmoji"
+                width="400px"
+              />
+            </v-menu>
+          </v-col>
+        </v-row>
       </v-col>
       <v-col cols="2" class="ma-0 pa-0 align-self-center ms-4">
         <v-btn
@@ -37,26 +70,60 @@
 export default {
   props: {
     post: Object,
-    index: Number,
     display: Boolean
   },
   data: () => ({
-    comment: ""
+    comment: "",
+    selectionStart: null,
+    selected: false
   }),
   methods: {
+    selectEmoji(emoji) {
+      let el = this.$refs.addCommentTextArea.$refs.input;
+
+      if (this.selected) this.selectionStart = el.selectionStart;
+      else if (!this.selected && this.selectionStart === null)
+        this.selectionStart = el.selectionStart;
+
+      this.comment =
+        this.comment.substring(0, el.selectionStart) +
+        this.comment.substring(el.selectionEnd);
+
+      let before = this.comment.substring(0, this.selectionStart);
+      let after = this.comment.substring(this.selectionStart);
+
+      this.comment = before + emoji.data + after;
+      this.selectionStart += 2;
+
+      this.selected = false;
+    },
+
     async addComment() {
       if (!this.comment.trim()) return (this.comment = "");
+
+      const api = `/feed/posts/${this.post._id}/comments`;
+      const data = { commentBody: this.comment };
       try {
-        const res = await this.$store.dispatch("addComment", {
-          comment: this.comment,
-          index: this.index,
-          id: this.post._id
-        });
-        this.comment = "";
+        const res = await this.$http.post(api, data);
+
+        if (res.status === 200) {
+          console.log("Comment added successfully");
+          this.post.totalComments++;
+          if (!this.post.comments) this.post.comments = [];
+          this.post.comments.push(res.data.comment);
+          this.comment = "";
+        }
       } catch (err) {
+        console.log("Something went wrong from:AddComment");
         console.log(err);
       }
     }
+  },
+  mounted() {
+    let el = this.$refs.addCommentTextArea.$refs.input;
+    el.onclick = () => {
+      this.selected = true;
+    };
   }
 };
 </script>
