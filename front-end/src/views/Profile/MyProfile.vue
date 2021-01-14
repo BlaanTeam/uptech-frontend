f126c66720e27ef01f9b7694ba6c112f93a16ae9
Abdelouahed Oumoussa <abdelwahd.oumouss@gmail.com>
2021-01-14 15:21:54 +0100

    Update: MyProfile.vue

--- front-end/src/views/Profile/MyProfile.vue
+++ front-end/src/views/Profile/MyProfile.vue
@@ -1,13 +1,43 @@
 <template>
-  <div class="my-profile">
-    my profile
+  <div class="my-profile bg">
+    <!-- <ProfileSkeleton /> -->
+    <Profile>
+      <Dialog>
+        <v-btn
+          class="ms-2 mt-n2 text-capitalize"
+          @click="log('Edit')"
+          color="primary"
+          dark
+          text
+        >
+          <v-icon left size="18">mdi-square-edit-outline</v-icon>
+          Edit
+        </v-btn>
+      </Dialog>
+    </Profile>
+    <Tabs />
   </div>
 </template>
 
 <script>
+import Profile from "@/components/Profile/Profile";
+import ProfileSkeleton from "@/components/Skeletons/ProfileSkeleton";
+import Tabs from "@/components/Profile/Tabs";
+import Dialog from "@/components/Profile/Dialog";
+
 export default {
-  name: "MyProfile"
+  name: "MyProfile",
+  components: { Profile, Tabs, Dialog /*ProfileSkeleton*/ },
+  methods: {
+    log(e) {
+      console.log(e + " clicked");
+    }
+  }
 };
 </script>
 
-<style scoped></style>
+<style lang="scss">
+.my-profile {
+  width: 56vw;
+}
+</style>
