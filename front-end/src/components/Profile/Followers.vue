260ede3123aec17f230f7f0d53ae5a285568481c
Abdelouahed Oumoussa <abdelwahd.oumouss@gmail.com>
2021-01-18 00:25:16 +0100

    add: Follower.vue and Following.vue

---
+++ front-end/src/components/Profile/Followers.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-dialog scrollable v-model="dialog" max-width="400px">
+    <template v-slot:activator="{ on, attrs }">
+      <span v-bind="attrs" v-on="on">
+        <slot></slot>
+      </span>
+    </template>
+    <v-card class="bg edit-profile">
+      <v-card-title>
+        <h1 class="title ms-2">Followers</h1>
+        <v-spacer></v-spacer>
+        <div>{{ followers }} User</div>
+      </v-card-title>
+      <v-card-text style="height: 80vh;">
+        <h2 class="my-4" v-for="i in 50" :key="i">
+          testing
+        </h2>
+      </v-card-text>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  props: {
+    userName: { type: String, required: true },
+    followers: { required: true }
+  },
+  data: () => ({
+    dialog: false
+  }),
+  mounted() {
+    console.log("edit post");
+  }
+};
+</script>
+
+<style>
+.edit-profile .v-card__title {
+  box-shadow: 0px 3px 10px rgba(0, 0, 0, 0.2);
+}
+</style>
