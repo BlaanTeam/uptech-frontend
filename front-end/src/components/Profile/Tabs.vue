6dcdbe2e072f3d795f0fd1bf172612557b418255
Abdelouahed Oumoussa <abdelwahd.oumouss@gmail.com>
2021-01-19 18:14:23 +0100

    Update: profille Tabs.vue

--- front-end/src/components/Profile/Tabs.vue
+++ front-end/src/components/Profile/Tabs.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="tabs mt-4 px-10">
+  <div class="tabs mt-8 px-10">
     <v-tabs v-model="tabs" centered grow background-color="bg">
       <v-tab>
         Posts
@@ -13,20 +13,33 @@
     </v-tabs>
 
     <v-tabs-items
-      class="auth-bg"
+      class="bg"
       v-model="tabs"
       v-if="
         userInfo.isPrivate && !userInfo.followedByViewer && !userInfo.isOwner
       "
     >
-      <div class="mt-4 text-center auth-bg">
-        <h1>Private account</h1>
+      <div class="mt-4 text-center bg">
+        <h3>Oops, You get Into a private area</h3>
+        <h4 class="font-weight-regular">
+          You can not see any info about this account
+        </h4>
+        <h4 class="font-weight-regular">Try to send a follow request</h4>
+        <PrivateSvg width="300" />
       </div>
     </v-tabs-items>
 
-    <v-tabs-items v-else v-model="tabs" class="auth-bg px-1">
+    <v-tabs-items
+      v-else
+      v-model="tabs"
+      class="px-1"
+      :class="{ bg: !posts.length, 'auth-bg': posts.length }"
+    >
       <v-tab-item>
-        <div v-if="posts.length" class="auth-bg pt-4 px-1">
+        <div v-if="!loaded">
+          <PostSkeletonVue v-for="i in 4" :key="i" />
+        </div>
+        <div v-if="loaded && posts.length" class="auth-bg pt-4 px-1">
           <span v-for="(post, index) in posts" :key="post._id">
             <Post
               :post="post"
@@ -36,8 +49,9 @@
             />
           </span>
         </div>
-        <div v-else>
-          <h1>No Posts yet</h1>
+        <div v-if="loaded && !posts.length" class="text-center py-4 bg">
+          <v-icon size="80" color="#b68d06">mdi-database-alert-outline</v-icon>
+          <h2>This user doesn't have any posts yet</h2>
         </div>
       </v-tab-item>
       <v-tab-item>
@@ -57,14 +71,19 @@
 <script>
 export default {
   name: "Tabs",
-  components: { Post: () => import("@/components/Post/Post") },
+  components: {
+    Post: () => import("@/components/Post/Post"),
+    PrivateSvg: () => import("@/components/svg/PrivateSvg"),
+    PostSkeletonVue: () => import("@/components/Skeletons/PostSkeleton")
+  },
   props: {
     userInfo: { type: Object, required: true }
   },
   data: () => ({
     tabs: null,
     posts: [],
-    pageInfo: {}
+    pageInfo: {},
+    loaded: false
   }),
   methods: {
     async getUserPosts() {
@@ -74,8 +93,8 @@ export default {
         if (res.status === 200) {
           this.posts = res.data.posts;
           this.pageInfo = res.data.pageInfo;
+          this.loaded = true;
 
-          console.log(res);
           return res.data.posts;
         }
       } catch (err) {
@@ -108,7 +127,7 @@ export default {
 .theme--light {
   .tabs {
     .v-tabs {
-      border-bottom: 1px solid #a0a0a0 !important;
+      border-bottom: 1px solid #e6b30d !important;
     }
   }
 }
