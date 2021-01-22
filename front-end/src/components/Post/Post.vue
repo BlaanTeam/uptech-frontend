f5990de9ceedfed48fc00caf737256672e99dd7a
Abdelouahed Oumoussa <abdelwahd.oumouss@gmail.com>
2021-01-22 15:47:46 +0100

    Update: Post.vue - update view post button

--- front-end/src/components/Post/Post.vue
+++ front-end/src/components/Post/Post.vue
@@ -35,18 +35,18 @@
           </p>
         </v-col>
         <v-spacer></v-spacer>
+
         <router-link :to="{ name: 'ViewPost', params: { postId: post._id } }">
-          <v-btn
-            height="28"
-            width="94"
-            class="me-2 text-capitalize caption view-post-btn"
-            text
-            dense
-          >
-            <v-icon left small>mdi-eye</v-icon>
-            view post
-          </v-btn>
+          <v-tooltip bottom color="#2F3136" nudge-top="8">
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn v-bind="attrs" v-on="on" class="view-post-btn" icon>
+                <v-icon>mdi-eye</v-icon>
+              </v-btn>
+            </template>
+            <span>View Post</span>
+          </v-tooltip>
         </router-link>
+
         <v-col lg="1" md="1" sm="2" class="pa-0 ma-0 text-center">
           <v-menu
             :attach="'.card-title' + index"
