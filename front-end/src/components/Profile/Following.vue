02cd1d475c61a0b4f48d83918bf115ea1b48abda
Abdelouahed Oumoussa <abdelwahd.oumouss@gmail.com>
2021-01-23 18:14:47 +0100

    Add: Popover profile to following and followers

--- front-end/src/components/Profile/Following.vue
+++ front-end/src/components/Profile/Following.vue
@@ -1,30 +1,52 @@
 <template>
   <v-dialog scrollable v-model="dialog.value" max-width="400px">
-    <v-card class="bg edit-profile">
-      <v-card-title>
+    <v-card class="bg following">
+      <v-card-title class="bg header">
         <h1 class="title d-inline-block float-left">Following</h1>
         <v-icon @click="dialog.value = false" class="ms-auto">
           mdi-close
         </v-icon>
       </v-card-title>
-      <v-card-text style="height: 80vh;" class="ps-3 py-3 px-0">
-        <span v-for="user in users" :key="user._id">
-          <div class="d-flex py-1">
-            <div class="align-self-center">
-              <img src="@/assets/images/avatar.svg" width="36" alt="Avatar" />
-            </div>
-            <div cols="6" class="align-sefl-center mx-3">
+      <div class="bg ps-3 pb-3 px-0 pt-16">
+        <div v-for="user in users" :key="user._id" class="d-flex py-1">
+          <div class="align-self-center">
+            <PopoverProfile
+              :userInfo="user"
+              :index="user._id"
+              :userName="user.userName"
+            >
+              <router-link
+                :to="{
+                  name: 'ViewProfile',
+                  params: { userName: user.userName }
+                }"
+              >
+                <img src="@/assets/images/avatar.svg" width="36" alt="Avatar" />
+              </router-link>
+            </PopoverProfile>
+          </div>
+          <div cols="6" class="align-sefl-center mx-3">
+            <router-link
+              :to="{
+                name: 'ViewProfile',
+                params: { userName: user.userName }
+              }"
+            >
               <h3 class="text-capitilize">
-                {{ user.profile.firstName }}
-                {{ user.profile.lastName }}
+                <span v-if="user.profile.firstName">
+                  {{ user.profile.firstName }}
+                </span>
+                <span v-if="user.profile.lastName">
+                  {{ user.profile.lastName }}
+                </span>
               </h3>
               <h5 class="mt-n2">@{{ user.userName }}</h5>
-            </div>
-            <div v-if="!user.isOwner" class="align-self-center ms-auto me-1">
-              <FollowUnfollow :userInfo="user" :myInfo="myInfo" />
-            </div>
+            </router-link>
           </div>
-        </span>
+          <div v-if="!user.isOwner" class="align-self-center ms-auto me-1">
+            <FollowUnfollow :userInfo="user" :myInfo="myInfo" />
+          </div>
+        </div>
         <infinite-loading @infinite="infiniteHandler">
           <template slot="no-results">
             <h2 class="mt-4 ps-2 pe-6 font-weight-regular">
@@ -32,16 +54,17 @@
             </h2>
           </template>
         </infinite-loading>
-      </v-card-text>
+      </div>
     </v-card>
   </v-dialog>
 </template>
 
 <script>
 import FollowUnfollow from "./FollowUnfollow";
+import PopoverProfile from "../Post/PopoverProfile";
 
 export default {
-  components: { FollowUnfollow },
+  components: { FollowUnfollow, PopoverProfile },
   props: { userName: String, myInfo: { required: false }, dialog: Object },
   data: () => ({
     users: [],
@@ -73,7 +96,16 @@ export default {
 </script>
 
 <style>
-.edit-profile .v-card__title {
+.following .header {
   box-shadow: 0px 3px 10px rgba(0, 0, 0, 0.2);
+  z-index: 205;
+  position: fixed;
+  margin: 0 auto;
+  width: 400px;
+}
+@media (max-width: 450px) {
+  .following .header {
+    width: 86vw;
+  }
 }
 </style>
