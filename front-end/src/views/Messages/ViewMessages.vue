a2750f28ed824d8356e33b2b8539cef485f2adf7
Abdelouahed Oumoussa <abdelwahd.oumouss@gmail.com>
2021-04-08 23:37:36 +0100

    Update: ViewMessages.vue page - add backend functionalities

--- front-end/src/views/Messages/ViewMessages.vue
+++ front-end/src/views/Messages/ViewMessages.vue
@@ -4,15 +4,13 @@
     <v-divider vertical></v-divider>
     <div
       v-if="!$route.params.id"
-      class="chat-box ms-auto px-10 d-flex flex-column justify-center align-center"
+      class="chat-box mx-auto px-10 d-flex flex-column justify-center align-center"
     >
       <h2>You don’t have any selected conversation</h2>
       <h4 class="my-1">
         Select a conversation from your existing messages, or start a new one.
       </h4>
-      <v-btn color="primary" class="mt-10">
-        start new conversation
-      </v-btn>
+      <NewConversation />
     </div>
     <router-view class="messages" :key="$route.fullPath"></router-view>
   </div>
@@ -20,8 +18,10 @@
 
 <script>
 import Conversations from "@/components/Chat/Conversations.vue";
+import NewConversation from "@/components/Chat/NewConversation.vue";
+
 export default {
-  components: { Conversations },
+  components: { Conversations, NewConversation },
   data: () => ({
     items: [
       {
@@ -66,6 +66,7 @@ export default {
   .conversations {
     width: 30%;
     height: 100%;
+    overflow-y: auto;
   }
   .messages {
     width: 70%;
