c9d5ac2dcb663d4410fa1f38365756ec1e70f82e
Abdelouahed Oumoussa <abdelwahd.oumouss@gmail.com>
2021-04-08 23:35:00 +0100

    Update: Conversations.vue add backend functionalities

--- front-end/src/components/Chat/Conversations.vue
+++ front-end/src/components/Chat/Conversations.vue
@@ -1,33 +1,40 @@
 <template>
-  <div class="conversations bg">
-    <div class="d-flex py-3 px-2">
-      <img src="@/assets/images/avatar.svg" width="40" />
-      <h1 class="ms-4 ">Chats</h1>
-      <div class="ms-auto me-4 align-self-center">
-        <v-btn icon>
-          <v-icon size="25">mdi-cog</v-icon>
-        </v-btn>
+  <div class="conversations bg pt-16">
+    <div class="bg conversations__header">
+      <div class="d-flex px-2 py-3">
+        <img src="@/assets/images/avatar.svg" width="40" />
+        <h1 class="ms-4 ">Chats</h1>
+        <div class="ms-auto me-4 align-self-center">
+          <v-btn icon>
+            <v-icon size="25">mdi-cog</v-icon>
+          </v-btn>
+        </div>
       </div>
+      <v-divider></v-divider>
     </div>
-    <v-divider></v-divider>
-    <v-list class="bg" height="80%">
+    <v-list class="bg " height="80%">
       <v-list-item-group dense>
-        <div v-for="(item, i) in conversations" :key="i">
-          <router-link :to="{ path: '/messages/' + i }">
+        <div v-for="conv in conversations" :key="conv._id">
+          <router-link :to="{ name: 'ViewMessages', params: { id: conv._id } }">
             <v-list-item class="py-1">
-              <v-list-item-avatar width="40" height="40">
-                <v-img :src="item.avatar"></v-img>
+              <v-list-item-avatar>
+                <img src="@/assets/images/avatar.svg" width="36" alt="Avatar" />
               </v-list-item-avatar>
 
               <v-list-item-content>
                 <v-list-item-title class="mb-1">
-                  {{ item.title }}
+                  <span>{{ conv.user.userName }}</span>
+                  <span class="ms-1">{{ conv.user.profile.lastName }}</span>
                 </v-list-item-title>
                 <v-list-item-subtitle class="pa-0 ma-0">
                   <span>
-                    {{ item.subtitle.slice(0, 30) + "..." }}
+                    {{ conv.lastMessage.content }}
                   </span>
-                  <span class="float-right">14h</span>
+                  <timeago
+                    class="float-right"
+                    :datetime="conv.timestamp"
+                    :auto-update="60"
+                  />
                 </v-list-item-subtitle>
               </v-list-item-content>
             </v-list-item>
@@ -42,14 +49,23 @@
 <script>
 export default {
   name: "Conversations",
-  data: () => ({}),
-  props: {
-    conversations: { type: Array, required: true }
+
+  computed: {
+    conversations() {
+      return this.$store.getters.conversations;
+    }
   },
-  mounted() {
-    console.log("hello from conversation");
+  created() {
+    let res = this.$store.dispatch("getConversations");
   }
 };
 </script>
 
-<style></style>
+<style>
+.conversations__header {
+  position: absolute;
+  top: 0;
+  width: 30%;
+  z-index: 2;
+}
+</style>
