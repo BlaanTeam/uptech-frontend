4e76af163981f2b3eb2236e0fb837a2021e397e8
Abdelouahed Oumoussa <abdelwahd.oumouss@gmail.com>
2021-04-11 19:58:58 +0000

    Add: Message.vue component

---
+++ front-end/src/components/Chat/Message.vue
@@ -0,0 +1,95 @@
+<template>
+  <div
+    @mouseenter="Menu = true"
+    @mouseleave="Menu = false"
+    v-if="message.isOwner"
+    class="message message__sent"
+    :data-read="!message.read"
+  >
+    <div class="message__sent__state ms-1">
+      <v-icon v-if="!message.read" small>mdi-check</v-icon>
+      <v-icon color="teal" v-else small>mdi-check-all</v-icon>
+    </div>
+    <div class="message__sent__content primary darken-2">
+      {{ message.content }}
+    </div>
+    <div class="message__sent__menu mx-1">
+      <span v-show="Menu"><MessageMenu :message="message"/></span>
+    </div>
+  </div>
+
+  <div
+    v-else
+    @mouseenter="Menu = true"
+    @mouseleave="Menu = false"
+    class="message message__received"
+  >
+    <div class="message__received__content secondarybg darken-1">
+      {{ message.content }}
+    </div>
+  </div>
+</template>
+
+<script>
+import MessageMenu from "./MessageMenu";
+export default {
+  name: "Message",
+  components: { MessageMenu },
+  data: () => ({
+    Menu: false
+  }),
+  props: {
+    message: { type: Object, required: true }
+  },
+  sockets: {
+    read(data) {
+      if (data.messageId === this.message._id && this.message.isOwner) {
+        this.message.read = true;
+      }
+    }
+  },
+  mounted() {
+    if (!this.message.isOwner) {
+      this.$socket.emit("mark-read", {
+        userId: this.$store.getters.getUserId,
+        messageId: this.message._id
+      });
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+.message {
+  clear: both;
+  margin: 1px 0;
+  width: 80%;
+
+  &__received {
+    display: flex;
+    float: left;
+    &__content {
+      padding: 10px;
+      border-radius: 0 10px 10px 10px;
+    }
+  }
+  &__sent {
+    display: flex;
+    flex-direction: row-reverse;
+    align-items: center;
+    float: right;
+    &__state {
+      width: 3%;
+    }
+    &__content {
+      max-width: 88%;
+      padding: 10px;
+      border-radius: 10px 0 10px 10px;
+      color: #d8d8d8;
+    }
+    &__menu {
+      width: 6%;
+    }
+  }
+}
+</style>
