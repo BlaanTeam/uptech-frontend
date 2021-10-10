8e158c5e284dd2460237e574d014db67e064f134
Abdelouahed Oumoussa <abdelwahd.oumouss@gmail.com>
2021-10-10 23:11:37 +0100

    Use secure websocket protocol on production

--- src/plugins/socketio.js
+++ src/plugins/socketio.js
@@ -2,11 +2,11 @@ import Vue from "vue";
 import store from "../store";
 import VueSocket from "vue-socket.io";
 import io from "socket.io-client";
-let baseURL = `${process.env.VUE_APP_API_HOST}:${process.env.VUE_APP_API_PORT}`;
+let baseURL = `${process.env.VUE_APP_WS}://${process.env.VUE_APP_API_HOST}:${process.env.VUE_APP_API_PORT}`;
 Vue.use(
   new VueSocket({
     debug: true,
-    connection: io(`ws://${baseURL}`, {
+    connection: io(baseURL, {
       autoConnect: false,
       extraHeaders: {
         "x-auth-token": store.getters.getToken
